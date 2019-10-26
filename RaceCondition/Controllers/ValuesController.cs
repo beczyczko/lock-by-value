4e82ea23072b267a14605e10b9c531261d1b92f7
Dominik <d.beczyczko@gmail.com>
2019-10-26 14:46:52 +0200

    created endpoint with code locked by value

---
+++ RaceCondition/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RaceCondition.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly object Lock = new object();
+
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "start";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromForm] string value, [FromForm] string callNo)
+        {
+            // we could get user id from HttpContext and use it in lock
+            using (new LockByValue(value))
+            {
+                _logger.LogCritical($"Enter endpoint: {value}, {callNo}");
+                _logger.LogCritical($"Processing: {value}, {callNo}");
+                _logger.LogCritical($"Leave endpoint: {value}, {callNo}");
+            }
+
+            // bad aproach - you block code for everyone
+            //            lock (Lock)
+            //            {
+            //                _logger.LogCritical($"Enter endpoint: {value}, {callNo}");
+            //                _logger.LogCritical($"Processing: {value}, {callNo}");
+            //                _logger.LogCritical($"Leave endpoint: {value}, {callNo}");
+            //            }
+        }
+    }
+
+}
---
+++ RaceCondition/LockByValue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+
+namespace RaceCondition
+{
+    public class LockByValue : IDisposable
+    {
+        public bool WasHandled;
+        private Mutex _mutex;
+
+        public LockByValue(string mutexId, int timeOut = -1)
+        {
+            InitMutex(mutexId);
+            try
+            {
+                if (timeOut < 0)
+                {
+                    WasHandled = _mutex.WaitOne(Timeout.Infinite, false);
+                }
+                else
+                {
+                    WasHandled = _mutex.WaitOne(timeOut, false);
+                }
+
+                if (WasHandled == false)
+                {
+                    throw new TimeoutException("Timeout waiting for exclusive access on SingleInstance");
+                }
+            }
+            catch (AbandonedMutexException)
+            {
+                WasHandled = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_mutex != null)
+            {
+                if (WasHandled)
+                {
+                    _mutex.ReleaseMutex();
+                }
+
+                _mutex.Close();
+            }
+        }
+
+        private void InitMutex(string mutexId)
+        {
+            _mutex = new Mutex(false, mutexId);
+
+            var allowEveryoneRule = new MutexAccessRule(
+                new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+                MutexRights.FullControl,
+                AccessControlType.Allow);
+            var securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+            _mutex.SetAccessControl(securitySettings);
+        }
+    }
+}
